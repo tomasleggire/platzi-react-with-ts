7518f9879a5edba00e65435b7c43466545504190
Tomás Leggire <tomasleggire@hotmail.com>
2023-05-20 20:20:30 -0300

    state con otros tipos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,21 +4,28 @@ import { RandomFox } from "../../components/RandomFox";
 
 const random = (): number => Math.floor(Math.random()*123)+1;
 
+type ImageItem = {
+    id: string,
+    url: string,
+  }
+
+const generateId = () => Math.random().toString(36).substr(2,9);
+
 export default function Home() {
 
-  const [images, setImages] = useState<string[]> ([
-    `https:\/\/randomfox.ca\/images\/${random()}.jpg`,
-    `https:\/\/randomfox.ca\/images\/${random()}.jpg`,
-    `https:\/\/randomfox.ca\/images\/${random()}.jpg`,
-    `https:\/\/randomfox.ca\/images\/${random()}.jpg`,
+  const [images, setImages] = useState<Array<ImageItem>> ([
+    { id: generateId(), url:`https:\/\/randomfox.ca\/images\/${random()}.jpg`},
+    { id: generateId(), url:`https:\/\/randomfox.ca\/images\/${random()}.jpg`},
+    { id: generateId(), url:`https:\/\/randomfox.ca\/images\/${random()}.jpg`},
+    { id: generateId(), url:`https:\/\/randomfox.ca\/images\/${random()}.jpg`},
   ]);
 
   return (
     <main>
       <h1 className="text-3xl font-bold underline">Hello Platzi</h1>
-      {images.map((image, index) => (
-        <div key={index} className="p-4">
-          <RandomFox image={image}/>
+      {images.map((image) => (
+        <div key={image.id} className="p-4">
+          <RandomFox image={image.url}/>
         </div>
       ))}
     </main>
