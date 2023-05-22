a1f796017b00c60a6b061d31385322aba3741f59
Tomás Leggire <tomasleggire@hotmail.com>
2023-05-22 11:10:30 -0300

    lazy loading con observadores

--- components/RandomFox.tsx
+++ components/RandomFox.tsx
@@ -1,4 +1,4 @@
-import { useRef } from "react";
+import { useRef, useEffect, useState } from "react";
 
 type Props = {
     image: string,
@@ -6,6 +6,32 @@ type Props = {
 
 export const RandomFox = (props: Props): JSX.Element => {
     const node = useRef<HTMLImageElement>(null); //Si no sabemos el valor inicial del ref, lo inicializamos con null, para evitar problemas con ts
+    const [src, setSrc] = useState("data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMzIwIiBoZWlnaHQ9IjMyMCIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB2ZXJzaW9uPSIxLjEiLz4=");
 
-    return <img ref={node}  width={320} height="auto" src={props.image} className="rounded"/>
+    useEffect(() => {
+        //Nuevo observador
+        const observer = new IntersectionObserver((entries) => {
+            entries.forEach((entry) => {
+                // onIntersection --> console.log
+                if (entry.isIntersecting) {
+                    setSrc(props.image);
+                };
+            });
+        });
+        //observe node
+        if (node.current) {
+            observer.observe(node.current);
+        };
+        //desconectar
+        return () => {
+            observer.disconnect();
+        }
+    }, [props.image])
+
+    return <img ref={node}  width={320} height="auto" src={src} style={style}/>
+}
+
+const style = {
+    borderRadius: '10px',
+    backgroundColor: 'gray',
 }
