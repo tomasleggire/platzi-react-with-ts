e10254f208fe4ad26102259a88e6b039180fa805
Tomás Leggire <tomasleggire@hotmail.com>
2023-05-22 10:45:12 -0300

    useRef con ts

--- components/RandomFox.tsx
+++ components/RandomFox.tsx
@@ -1,7 +1,11 @@
+import { useRef } from "react";
+
 type Props = {
     image: string,
 };
 
 export const RandomFox = (props: Props): JSX.Element => {
-    return <img width={320} height="auto" src={props.image} className="rounded"/>
+    const node = useRef<HTMLImageElement>(null); //Si no sabemos el valor inicial del ref, lo inicializamos con null, para evitar problemas con ts
+
+    return <img ref={node}  width={320} height="auto" src={props.image} className="rounded"/>
 }
